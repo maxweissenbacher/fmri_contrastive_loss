fd7ad7018ee7c97ccd6ff35773ee120485abf2f0
maxweissenbacher <max.weissenbacher@proton.me>
2023-12-07 13:45:37 +0000

    Can now test different combinations of features

--- tuning/feature_tuning.py
+++ tuning/feature_tuning.py
@@ -9,13 +9,26 @@ import optuna
 from training.trainer import Trainer
 from models.nn import Net
 from metrics.evaluation import compute_eval_metrics
+import itertools
+from warnings import simplefilter
 
 
-def objective(trial, batch_size=512, num_epochs=2500):
+MAX_NUM_COMBINED_FEATURES = 3  # We consider combinations of up to this many features
+
+
+def create_feature_combinations(n):
+    feature_names = ['mean', 'var', 'std', 'skew', 'kurt', 'ar1', 'ar2', 'ar3']
+    feature_combinations = []
+    for i in range(1, n + 1):
+        feature_combinations += list(itertools.combinations(feature_names, i))
+    return feature_combinations
+
+
+def objective(trial, batch_size=512, num_epochs=2000):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    name = trial.suggest_categorical('feature', ['mean', 'var', 'std', 'skew', 'kurt', 'ar1', 'ar2', 'ar3'])
-    feature_names = [name]
+    feature_combinations = create_feature_combinations(MAX_NUM_COMBINED_FEATURES)
+    feature_names = trial.suggest_categorical('feature', feature_combinations)
+    num_features = len(feature_names)
 
     # Load data
     data = load_features("data", feature_names)
@@ -23,7 +36,7 @@ def objective(trial, batch_size=512, num_epochs=2500):
 
     # Hyperparameters
     model_params = {
-        'dim': 360,
+        'dim': 360 * num_features,
         'width': 64,
         'depth': 2,
         'nenc': 1,
@@ -63,13 +76,17 @@ def objective(trial, batch_size=512, num_epochs=2500):
 
 
 def feature_tuning():
-    search_space = {'feature': ['mean', 'var', 'std', 'skew', 'kurt', 'ar1', 'ar2', 'ar3']}
+    # Suppress UserWarnings from Optuna  #rogueCoding
+    simplefilter("ignore", category=UserWarning)
+
+    feature_combinations = create_feature_combinations(MAX_NUM_COMBINED_FEATURES)
+    search_space = {'feature': feature_combinations}
     study = optuna.create_study(
         direction="maximize",
         study_name="Feature tuning",
         sampler=optuna.samplers.GridSampler(search_space)
     )
-    study.optimize(objective, n_trials=5, timeout=72000)  # Timeout is in seconds, 20 hours
+    study.optimize(objective, n_trials=len(feature_combinations), timeout=72000)  # Timeout is in seconds, 20 hours
 
     filename = "./tuning/feature_tuning_study.pkl"
     with open(filename, "wb") as f:
