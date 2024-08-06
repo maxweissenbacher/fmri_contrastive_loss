3db5a0da0c4630dce99c6c828113e35c998ceca0
maxweissenbacher <max.weissenbacher@proton.me>
2024-08-06 14:09:26 +0100

    added code for learning latent dim from structural

--- learn_latent_from_structural/train.py
+++ learn_latent_from_structural/train.py
@@ -2,6 +2,11 @@ import numpy as np
 import torch
 from pathlib import Path
 from models.nn import Net
+from data.dataloading import load_features
+from models.linear import LinearLayer
+from torch.nn import MSELoss
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
     # Load structural data
@@ -10,6 +15,7 @@ if __name__ == '__main__':
     file_path = (cwd.parent / rel_path).resolve()
     with open(file_path, "rb") as f:
         structural = np.loadtxt(f)  # shape 999 x something
+        structural = torch.tensor(structural, dtype=torch.float32)
 
     # Load trained model
     name = 'mean'  # mean, std, var or ar1
@@ -30,6 +36,32 @@ if __name__ == '__main__':
         net.load_state_dict(torch.load(f, map_location=torch.device('cpu')))
         net.eval()
 
+    # Load data
+    data = load_features("../data", [name])
 
+    # Compute latent space
+    latent_dimension = net(data['features'])
+
+    # Compute mean across scans for each subject
+    latent_mean = latent_dimension.view(999, 4).mean(dim=-1, keepdim=True).detach()
 
     # Try to reproduce output from trained model from structural data
+    if not len(structural.shape) == 2:
+        raise ValueError(f"Structural data must be of shape num_samples x D where D is an integer. Got {structural.shape}")
+    model = LinearLayer(in_dim=structural.shape[-1], out_dim=1)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    criterion = MSELoss()
+    losses = []
+
+    for _ in range(250):
+        output = model(structural)
+        loss = criterion(output, latent_mean)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.detach().item())
+
+    plt.plot(losses)
+    plt.xlabel("Epochs")
+    plt.ylabel("MSE")
+    plt.savefig('losses.png')
